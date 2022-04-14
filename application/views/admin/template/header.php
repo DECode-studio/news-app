4ea174d5bdb3d7e020084669ba8bfe85d54c2f7f
Wahyu Gamma Gene <wahyu0645@gmail.com>
2022-04-14 16:22:00 +0700

    edit

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -35,7 +35,7 @@
 		<div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link text-white active bg-gradient-primary" href="<?php echo base_url('admin/dashboard'); ?>">
+					<a class="nav-link text-white <?php echo $this->uri->segment(2) == 'dashboard' ? 'active bg-gradient-primary' : '' ?>" href="<?php echo base_url('admin/dashboard'); ?>">
 						<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
 							<i class="material-icons opacity-10">dashboard</i>
 						</div>
@@ -43,7 +43,7 @@
 					</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link text-white " href="<?php echo base_url('admin/news'); ?>">
+					<a class="nav-link text-white <?php echo $this->uri->segment(2) == 'news' ? 'active bg-gradient-primary' : '' ?>" href="<?php echo base_url('admin/news'); ?>">
 						<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
 							<i class="material-icons opacity-10">table_view</i>
 						</div>
