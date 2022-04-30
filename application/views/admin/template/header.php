ba4ada47f69295a1fe1b8f7ab79a494a1062002a
Wahyu Gamma Gene <wahyu0645@gmail.com>
2022-04-30 22:02:33 +0700

    EDIT NEWS

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -140,7 +140,7 @@
 								</a>
 								</li>
 								<li class="mb-2">
-								<a class="dropdown-item border-radius-md" href="javascript:;">
+								<a class="dropdown-item border-radius-md" href="<?php echo base_url(''); ?>">
 									<div class="d-flex">
 										<i class="material-icons opacity-10 m-2">logout</i>
 										<h6 class="text-sm font-weight-normal m-2">
--- application/views/admin/view_news.php
+++ application/views/admin/view_news.php
@@ -63,6 +63,7 @@
 						<h6 class="text-white text-capitalize ps-3">News List</h6>
 					</div>
 				</div>
+				<!-- Card News List -->
 				<div class="card m-3" style="max-width: 100%;">
 					<div class="row g-0">
 						<div class="col-md-3">
@@ -104,12 +105,97 @@
 						<div class="col-md-9">
 						<div class="card-body">
 							<h5 class="card-title">Seminar Nasional 2K22 Kesehatan Mental</h5>
-							<span class="card-text">Author:<span class="text-dark font-weight-bold ms-sm-2">Bambang</span></span><br>
-							<span class="card-text">Date time: <span class="text-dark ms-sm-2 font-weight-bold">Senin, 05 November 2022</span></span><br>
-							<span class="card-text">News body: </span>
-							<div class="text-dark font-weight-bold text-sm news">
-								Lorem ipsum dolor sit amet amet jamet met ajay banakin baki mati sama bapaknye berantem ga jelas saling kuat kuatan ga danta pokoknya bodo ah Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus unde velit porro rem facere, vitae in, id amet tempora non aperiam eveniet neque aspernatur libero dolorum omnis ipsa maiores ea.
+							<table class="table table-borderless">
+								<tbody>
+									<tr>
+										<th>Author</th>
+										<th><span class="text-dark font-weight-bold">Bambang</span></th>
+									</tr>
+									<tr>
+										<th>Date Time</th>
+										<th><span class="text-dark font-weight-bold">Senin, 05 November 2022</span></th>
+									</tr>
+									<tr>
+										<th>News Body</th>
+										<th>
+											<div class="text-dark font-weight-bold news">
+											Lorem ipsum dolor sit amet amet jamet met ajay banakin baki mati sama bapaknye berantem ga jelas saling kuat kuatan ga danta pokoknya bodo ah Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus unde velit porro rem facere, vitae in, id amet tempora non aperiam eveniet neque aspernatur libero dolorum omnis ipsa maiores ea.
+											</div>
+										</th>
+									</tr>
+								</tbody>
+							</table>
+							<br>
+							<div class="ms-auto text-end">
+								<a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">delete</i>Delete</a>
+								<a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">edit</i>Edit</a>
 							</div>
+						</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="card m-3" style="max-width: 100%;">
+					<div class="row g-0">
+						<div class="col-md-3">
+							<div id="cr-uko" class="carousel slide img-pro" style="margin: auto; margin-top: 10px; margin-bottom: 10px;" data-bs-ride="carousel">
+								<div class="carousel-indicators">
+									<button type="button" data-bs-target="#cr-uko" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+									<button type="button" data-bs-target="#cr-uko" data-bs-slide-to="1" aria-label="Slide 2"></button>
+									<button type="button" data-bs-target="#cr-uko" data-bs-slide-to="2" aria-label="Slide 3"></button>
+									<button type="button" data-bs-target="#cr-uko" data-bs-slide-to="3" aria-label="Slide 4"></button>
+									<button type="button" data-bs-target="#cr-uko" data-bs-slide-to="4" aria-label="Slide 5"></button>
+								</div>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img src="https://decode-studio.github.io/wahid.github.io/public/images/uko/1.jpg" class="d-block img-pro">
+									</div>
+									<div class="carousel-item">
+										<img src="https://decode-studio.github.io/wahid.github.io/public/images/uko/4.jpg" class="d-block img-pro">
+									</div>
+									<div class="carousel-item">
+										<img src="https://decode-studio.github.io/wahid.github.io/public/images/uko/5.jpg" class="d-block img-pro">
+									</div>
+									<div class="carousel-item">
+										<img src="https://decode-studio.github.io/wahid.github.io/public/images/uko/7.jpg" class="d-block img-pro">
+									</div>
+									<div class="carousel-item">
+										<img src="https://decode-studio.github.io/wahid.github.io/public/images/uko/8.jpg" class="d-block img-pro">
+									</div>
+								</div>
+								<button class="carousel-control-prev" type="button" data-bs-target="#cr-uko" data-bs-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="visually-hidden">Previous</span>
+								</button>
+								<button class="carousel-control-next" type="button" data-bs-target="#cr-uko" data-bs-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="visually-hidden">Next</span>
+								</button>
+							</div>
+						</div>
+						<div class="col-md-9">
+						<div class="card-body">
+							<h5 class="card-title">Seminar Nasional 2K22 Kesehatan Mental</h5>
+							<table class="table table-borderless">
+								<tbody>
+									<tr>
+										<th>Author</th>
+										<th><span class="text-dark font-weight-bold">Bambang</span></th>
+									</tr>
+									<tr>
+										<th>Date Time</th>
+										<th><span class="text-dark font-weight-bold">Senin, 05 November 2022</span></th>
+									</tr>
+									<tr>
+										<th>News Body</th>
+										<th>
+											<div class="text-dark font-weight-bold news">
+											Lorem ipsum dolor sit amet amet jamet met ajay banakin baki mati sama bapaknye berantem ga jelas saling kuat kuatan ga danta pokoknya bodo ah Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus unde velit porro rem facere, vitae in, id amet tempora non aperiam eveniet neque aspernatur libero dolorum omnis ipsa maiores ea.
+											</div>
+										</th>
+									</tr>
+								</tbody>
+							</table>
 							<br>
 							<div class="ms-auto text-end">
 								<a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">delete</i>Delete</a>
