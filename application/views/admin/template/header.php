8abf06c8b42bb055294ac3196c555c11274a50f5
Wahyu Gamma Gene <wahyu0645@gmail.com>
2022-05-07 00:32:00 +0700

    TAMBAH MODAL

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -20,6 +20,7 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
 	<!-- CSS Files -->
 	<link id="pagestyle" href="<?php echo base_url('public/css/material-dashboard.css'); ?>" rel="stylesheet" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 	<link href="<?php echo base_url('public/css/style.css'); ?>" rel="stylesheet" />
 
