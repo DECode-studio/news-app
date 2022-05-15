6fea1fe21b6197019ca7d2bd22bfc6db2274da8b
DECode.studio <decode.std@gmail.com>
2022-05-15 22:04:05 +0700

    add crud curiculum

--- application/controllers/Curiculum.php
+++ application/controllers/Curiculum.php
@@ -8,16 +8,59 @@ class Curiculum extends CI_Controller
 {
     public function addCuriculum()
     {
-        # code...
+        if (isset($_SESSION['status']) && $_SESSION['status'] == "login") {
+
+            $date = date('Ymd.His');
+            $curiculum_id = "Curiculum.ID_" . $date;
+
+            $data = array(
+                'curiculum_id' => $curiculum_id,
+                'curiculum_name' => $this->input->post('txt_name'),
+                'curiculum_sks' => $this->input->post('txt_sks')
+            );
+
+            $this->db->insert('tbl_curiculum', $data);
+
+            redirect('admin/curiculum');
+        } else {
+            redirect('admin/login');
+        }
     }
 
     public function updateCuriculum()
     {
-        # code...
+
+        if (isset($_SESSION['status']) && $_SESSION['status'] == "login") {
+
+            $curiculum_id = $this->input->post('txt_edit_id');
+
+            $data = array(
+                'curiculum_id' => $curiculum_id,
+                'curiculum_name' => $this->input->post('txt_edit_name'),
+                'curiculum_sks' => $this->input->post('txt_edit_sks')
+            );
+
+            $this->db->where('curiculum_id', $curiculum_id);
+            $this->db->update('tbl_curiculum', $data);
+
+            redirect('admin/curiculum');
+        } else {
+            redirect('admin/login');
+        }
     }
 
-    public function deleteCuriculum()
+    public function deleteCuriculum($curiculum_id)
     {
-        # code...
+        if (isset($_SESSION['status']) && $_SESSION['status'] == "login") {
+
+            $where = array('curiculum_id' => $curiculum_id);
+
+            $this->db->delete('tbl_curiculum', $where);
+            $this->db->delete('tbl_subject', $where);
+
+            redirect('admin/curiculum');
+        } else {
+            redirect('admin/login');
+        }
     }
 }
--- application/views/admin/view_curiculum.php
+++ application/views/admin/view_curiculum.php
@@ -57,10 +57,10 @@
                                         </td>
                                         <td class="align-middle">
                                             <div class="ms-auto text-end">
-                                                <a type="submit" class="btn btn-link text-danger text-gradient px-3 mb-0" href="">
+                                                <a type="submit" class="btn btn-link text-danger text-gradient px-3 mb-0" href="<?= base_url('curiculum/deletecuriculum/' . $curiculum_data['curiculum_id']) ?>">
                                                     <i class="material-icons text-sm me-2">delete</i>Delete
                                                 </a>
-                                                <a class="btn btn-link text-dark px-3 mb-0" data-bs-toggle="modal" data-bs-target="#frm_update_lecture" data-bs-whatever="@mdo" onclick="">
+                                                <a class="btn btn-link text-dark px-3 mb-0" data-bs-toggle="modal" data-bs-target="#frm_update_curiculum" data-bs-whatever="@mdo" onclick="showData('<?= $curiculum_data['curiculum_id']; ?>', '<?= $curiculum_data['curiculum_name']; ?>', '<?= $curiculum_data['curiculum_sks']; ?>')">
                                                     <i class="material-icons text-sm me-2">edit</i>Edit
                                                 </a>
                                             </div>
@@ -75,8 +75,89 @@
         </div>
     </div>
 </div>
+
 <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2" data-bs-toggle="modal" data-bs-target="#frm_add_curiculum" data-bs-whatever="@mdo" onclick="">
         <i class="material-icons py-2" style="font-size: 30px;">add</i>
     </a>
 </div>
+
+<div class="modal fade" id="frm_add_curiculum" tabindex="-1" aria-labelledby="frm_add_curiculum" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="frm_add_curiculum">Add Curiculum</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form role="form" method='post' action="<?= base_url('curiculum/addcuriculum') ?>" enctype='multipart/form-data'>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="txt_name" class="form-label">Curiculum's name</label>
+                        <div class="input-group input-group-outline">
+                            <input type="text" class="form-control" id="txt_name" name="txt_name">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="txt_sks" class="form-label">Number of SKS</label>
+                        <div class="input-group input-group-outline">
+                            <input type="number" class="form-control" id="txt_sks" name="txt_sks" max="3" min="1" value="1">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="frm_update_curiculum" tabindex="-1" aria-labelledby="frm_update_curiculum" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="frm_update_curiculum">Edit Curiculum</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form role="form" method='post' action="<?= base_url('curiculum/updatecuriculum') ?>" enctype='multipart/form-data'>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="txt_edit_id" class="form-label">Curiculum's name</label>
+                        <div class="input-group input-group-outline">
+                            <input type="text" class="form-control" id="txt_edit_id" name="txt_edit_id">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="txt_edit_name" class="form-label">Curiculum's name</label>
+                        <div class="input-group input-group-outline">
+                            <input type="text" class="form-control" id="txt_edit_name" name="txt_edit_name">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="txt_edit_sks" class="form-label">Number of SKS</label>
+                        <div class="input-group input-group-outline">
+                            <input type="number" class="form-control" id="txt_edit_sks" name="txt_edit_sks" max="3" min="1" value="1">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Edit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    var txt_edit_id = document.getElementById('txt_edit_id')
+    var txt_edit_name = document.getElementById('txt_edit_name')
+    var txt_edit_sks = document.getElementById('txt_edit_sks')
+
+    function showData(curiculumId, curiculumName, curiculumSKS) {
+        txt_edit_id.value = curiculumId;
+        txt_edit_name.value = curiculumName;
+        txt_edit_sks.value = curiculumSKS;
+    }
+</script>
