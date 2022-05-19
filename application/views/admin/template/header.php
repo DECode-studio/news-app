d9e38d0b8e9ee3223ece0bfd23a49c23b54c764e
Wahyu Gamma Gene <wahyu0645@gmail.com>
2022-05-19 10:07:22 +0700

    update page news

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -148,7 +148,7 @@
 										</a>
 									</li>
 									<li class="mb-2">
-										<a class="dropdown-item border-radius-md" href="<?php echo base_url('admin/logout'); ?>">
+										<a class="dropdown-item border-radius-md" href="<?php echo base_url('admin/logout'); ?>?logout='1'">
 											<div class="d-flex">
 												<i class="material-icons opacity-10 m-2">logout</i>
 												<h6 class="text-sm font-weight-normal m-2">
